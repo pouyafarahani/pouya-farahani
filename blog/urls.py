068ec1e3394f3b6f4459ffbd0aa8ebb8b5f4a8bd
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-05-04 23:00:47 +0330

    add file fonts and config navbar proje

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from .views import BlogListView, BlogDetailView
 
 app_name = 'blog'
 
 urlpatterns = [
-
+    path('list', BlogListView.as_view(), name='blog_list'),
+    path('list', BlogDetailView.as_view(), name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class BlogListView(View):
+    def get(self, request):
+        return render(request, 'blog/blog_list.html')
+
+
+class BlogDetailView(View):
+    def get(self, request):
+        return render(request, 'blog/blog_detail.html')
