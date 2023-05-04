c0ec6f4a913bab3b19f987aaa00f319019d2ad17
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-05-04 21:40:28 +0330

    add file home

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
